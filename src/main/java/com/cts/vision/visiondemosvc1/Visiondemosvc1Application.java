7c08025abcef4c645a154bc564d8ae9bd837e5c6
Saibal Kumar Ghosh <saibal.ghosh.1980@gmail.com>
2022-06-01 12:52:38 +0100

    NA

---
+++ src/main/java/com/cts/vision/visiondemosvc1/Visiondemosvc1Application.java
@@ -0,0 +1,13 @@
+package com.cts.vision.visiondemosvc1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Visiondemosvc1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Visiondemosvc1Application.class, args);
+	}
+
+}
